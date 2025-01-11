54d2e7f95ab623ab4e703ce601ec8f852a8ca976
Gagandeep <137174182+06Gagan@users.noreply.github.com>
2025-01-11 05:38:49 +0530

    added database

--- config/db.js
+++ config/db.js
@@ -1,19 +1,15 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
-  .then(() => {
-    console.log("Connected to PostgreSQL database");
-  })
-  .catch((err) => {
-    console.error("Connection error:", err);
-  });
+  .then(() => console.log("Connected to PostgreSQL database"))
+  .catch((err) => console.error("Connection error:", err));
 
 module.exports = pool;
+
--- models/admin.js
+++ models/admin.js
@@ -1,16 +1,9 @@
-const { Pool } = require("pg");
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-});
+const db = require("../config/db");
 
 // Get admin password
 const getAdminPassword = async () => {
   const query = "SELECT password FROM admin WHERE id = 1";
-  const result = await pool.query(query);
+  const result = await db.query(query);
   return result.rows[0]?.password || null;
 };
 
@@ -21,7 +14,7 @@ const updateAdminPassword = async (hashedPassword) => {
     VALUES (1, $1)
     ON CONFLICT (id)
     DO UPDATE SET password = $1`;
-  await pool.query(query, [hashedPassword]);
+  await db.query(query, [hashedPassword]);
 };
 
 module.exports = {
